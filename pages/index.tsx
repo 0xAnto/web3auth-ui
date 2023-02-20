ff51ae87ec9bbae3aeb88acb55121c6f0b0b22ae
0xAnto <antonyranjith17@gmail.com>
2023-02-20 13:02:27 +0530

    added web3auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,249 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Web3Auth } from '@web3auth/modal'
+import RPC from './api/aptosRPC'
+import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from '@web3auth/base'
+
+const clientId =
+  'BEglQSgt4cUWcj6SKRdu5QkOXTsePmMcusG5EAoyjyOYKlVRjIF1iCNnMOTfpzCiunHRrMui8TIwQPXdkQ8Yxuk' // get from https://dashboard.web3auth.io
 
 const Home: NextPage = () => {
+  const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null)
+  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(
+    null
+  )
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3auth = new Web3Auth({
+          clientId,
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.OTHER,
+            chainId: '0x1',
+            rpcTarget: 'https://rpc.ankr.com/eth', // This is the public RPC we have added, please pass on your own endpoint while creating an app
+          },
+          uiConfig: {
+            theme: 'dark',
+            loginMethodsOrder: ['google', 'github'],
+            defaultLanguage: 'en',
+            appLogo: 'https://web3auth.io/images/w3a-L-Favicon-1.svg', // Your App Logo Here
+          },
+          web3AuthNetwork: 'cyan',
+        })
+
+        setWeb3auth(web3auth)
+
+        await web3auth.initModal()
+        if (web3auth.provider) {
+          setProvider(web3auth.provider)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    init()
+  }, [])
+
+  const login = async () => {
+    if (!web3auth) {
+      uiConsole('web3auth not initialized yet')
+      return
+    }
+    const web3authProvider = await web3auth.connect()
+    setProvider(web3authProvider)
+  }
+
+  const authenticateUser = async () => {
+    if (!web3auth) {
+      uiConsole('web3auth not initialized yet')
+      return
+    }
+    const idToken = await web3auth.authenticateUser()
+    uiConsole(idToken)
+  }
+
+  const getUserInfo = async () => {
+    if (!web3auth) {
+      uiConsole('web3auth not initialized yet')
+      return
+    }
+    const user = await web3auth.getUserInfo()
+    uiConsole(user)
+  }
+
+  const logout = async () => {
+    if (!web3auth) {
+      uiConsole('web3auth not initialized yet')
+      return
+    }
+    await web3auth.logout()
+    setProvider(null)
+  }
+
+  const getAccounts = async () => {
+    if (!provider) {
+      uiConsole('provider not initialized yet')
+      return
+    }
+    const rpc = new RPC(provider)
+    const address = await rpc.getAccounts()
+    uiConsole(address)
+  }
+
+  const getBalance = async () => {
+    if (!provider) {
+      uiConsole('provider not initialized yet')
+      return
+    }
+    const rpc = new RPC(provider)
+    const balance = await rpc.getBalance()
+    uiConsole(balance)
+  }
+
+  const getAirdrop = async () => {
+    if (!provider) {
+      uiConsole('provider not initialized yet')
+      return
+    }
+    const rpc = new RPC(provider)
+    const airdrop = await rpc.getAirdrop()
+    uiConsole('Airdropped some tokens TxID: ' + airdrop)
+  }
+
+  const sendTransaction = async () => {
+    if (!provider) {
+      uiConsole('provider not initialized yet')
+      return
+    }
+    const rpc = new RPC(provider)
+    const receipt = await rpc.sendTransaction()
+    uiConsole('TxID: ' + receipt)
+  }
+
+  const getPrivateKey = async () => {
+    if (!provider) {
+      uiConsole('provider not initialized yet')
+      return
+    }
+    const rpc = new RPC(provider)
+    const privateKey = await rpc.getPrivateKey()
+    uiConsole(privateKey)
+  }
+
+  function uiConsole(...args: any[]): void {
+    const el = document.querySelector('#console>p')
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2)
+    }
+  }
+
+  const loggedInView = (
+    <>
+      <div className="flex-container">
+        <div>
+          <button onClick={getUserInfo} className="card">
+            Get User Info
+          </button>
+        </div>
+        <div>
+          <button onClick={authenticateUser} className="card">
+            Get ID Token
+          </button>
+        </div>
+        <div>
+          <button onClick={getAccounts} className="card">
+            Get Accounts
+          </button>
+        </div>
+        <div>
+          <button onClick={getBalance} className="card">
+            Get Balance
+          </button>
+        </div>
+        <div>
+          <button onClick={getAirdrop} className="card">
+            Get Airdrop
+          </button>
+        </div>
+        <div>
+          <button onClick={sendTransaction} className="card">
+            Send Transaction
+          </button>
+        </div>
+        <div>
+          <button onClick={getPrivateKey} className="card">
+            Get Private Key
+          </button>
+        </div>
+        <div>
+          <button onClick={logout} className="card">
+            Log Out
+          </button>
+        </div>
+      </div>
+      <div id="console" style={{ whiteSpace: 'pre-line' }}>
+        <p style={{ whiteSpace: 'pre-line' }}>Logged in Successfully!</p>
+      </div>
+    </>
+  )
+
+  const unloggedInView = (
+    <button onClick={login} className="card">
+      Login
+    </button>
+  )
+
+  // return (
+  //   <div className="container">
+  //     <h1 className="title">
+  //       <a target="_blank" href="http://web3auth.io/" rel="noreferrer">
+  //         Web3Auth{" "}
+  //       </a>
+  //       & ReactJS Aptos Example
+  //     </h1>
+
+  //     <div className="grid">{provider ? loggedInView : unloggedInView}</div>
+
+  //     <footer className="footer">
+  //       <a
+  //         href="https://github.com/Web3Auth/examples/tree/main/web-modal-sdk/aptos/react-aptos-web3auth-example"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Source code
+  //       </a>
+  //     </footer>
+  //   </div>
+  // );
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Web3auth</title>
         <meta name="description" content="Generated by create next app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 className="title">
+          <a target="_blank" href="http://web3auth.io/" rel="noreferrer">
+            Web3Auth{' '}
+          </a>
+          & ReactJS Aptos Example
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className="grid">{provider ? loggedInView : unloggedInView}</div>
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -64,7 +254,7 @@ const Home: NextPage = () => {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer>
+      </footer> */}
     </div>
   )
 }
